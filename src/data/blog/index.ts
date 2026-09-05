203ea164fb548498f5fb6ec14d3bd33e5bfff720
sauravsa-world <sauravsha51@gmail.com>
2026-09-06 00:44:14 +0530

    Add new blog post import for WhatsApp marketing guide

--- src/data/blog/index.ts
+++ src/data/blog/index.ts
@@ -30,11 +30,12 @@ import { post as p27 } from "./posts/google-algorithm-updates-explained";
 import { post as p28 } from "./posts/digital-marketing-trends-in-bihar";
 import { post as p29 } from "./posts/ultimate-local-seo-checklist";
 import { post as p30 } from "./posts/complete-digital-marketing-guide-for-bihar-businesses";
-
+import { post as p31 } from "./posts/whatsapp-marketing-guide-bihar";
 export const blogPosts: BlogPost[] = [
   p01, p02, p03, p04, p05, p06, p07, p08, p09, p10,
   p11, p12, p13, p14, p15, p16, p17, p18, p19, p20,
   p21, p22, p23, p24, p25, p26, p27, p28, p29, p30,
+  p31,
 ].sort((a, b) => (a.publishDate < b.publishDate ? 1 : -1));
 
 export function getPost(slug: string) {
